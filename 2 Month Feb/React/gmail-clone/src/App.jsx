fbf1543c4afbf2575492b6906430d1451aaa155f
manishavhanmane00-hash <manishavhanmane00@gmail.com>
2026-02-10 16:40:47 +0530

    done with all changes

---
+++ 2 Month Feb/React/gmail-clone/src/App.jsx
@@ -0,0 +1,40 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center bg-danger"> HOC </h1>
+
+      <p>React is a JavaScript library for building user interfaces.
+        React is used to build single-page applications.
+        React allows us to create reusable UI components.
+      </p>
+      <p>
+        React is a JavaScript library for building user interfaces.
+        React is used to build single-page applications.
+        React allows us to create reusable UI components.
+      </p>
+      <p>
+        React is a JavaScript library for building user interfaces.
+        React is used to build single-page applications.
+        React allows us to create reusable UI components.
+      </p>
+
+      <ul>
+        <li>
+          Home
+        </li>
+        <li>
+          About Us
+        </li>
+        <li>
+          Contact Us
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default App
