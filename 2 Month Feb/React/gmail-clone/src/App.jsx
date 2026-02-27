75f19594063ba1ee23a7e95f14666e22a9828b74
manishavhanmane00-hash <manishavhanmane00@gmail.com>
2026-02-28 01:42:42 +0530

    App.jsx

--- 2 Month Feb/React/gmail-clone/src/App.jsx
+++ 2 Month Feb/React/gmail-clone/src/App.jsx
@@ -781,42 +781,29 @@ function App() {
                 </div>
               </div>
               <div className='box-2'>
-                <div className='header-icon-2'>
-                  
+                <div className='header-icon-2'>                  
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="blue" class="bi bi-calendar" viewBox="0 0 16 16">
                       <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
-                    </svg>
-                  
+                    </svg> 
                 </div>
-
-                <div className='header-icon-2'>
-                  
+                <div className='header-icon-2'>                  
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="yellow" class="bi bi-device-hdd-fill" viewBox="0 0 16 16">
                       <path d="M8.785 9.896A3.001 3.001 0 0 0 8 4a3 3 0 0 0-.891 5.865c.667-.44 1.396-.91 1.955-1.268.224-.144.483.115.34.34zM9 7a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
                       <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm9 1.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m0 13a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m-9.5.5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1M4 1.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m2.882 11.177a1.102 1.102 0 0 1-1.56-1.559c.1-.098.396-.314.795-.588a4 4 0 1 1 1.946.47c-.537.813-1.02 1.515-1.181 1.677" />
-                    </svg>
-                  
+                    </svg>                  
                 </div>
-
-                <div className='header-icon-2'>
-                 
+                <div className='header-icon-2'>                 
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="blue" class="bi bi-check2-circle" viewBox="0 0 16 16">
                       <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0" />
                       <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z" />
-                    </svg>
-                 
+                    </svg>                
                 </div>
-
-                <div className='header-icon-2'>
-                 
+                <div className='header-icon-2'>                 
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="blue" class="bi bi-person-fill" viewBox="0 0 16 16">
                       <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-                    </svg>
-                 
+                    </svg>                 
                 </div>
-
-                <div className='header-icon-2'>
-                  
+                <div className='header-icon-2'>                  
                     <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="black" class="bi bi-plus" viewBox="0 0 16 16">
                       <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
                     </svg>
