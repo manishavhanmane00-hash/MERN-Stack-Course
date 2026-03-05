79814044d5dba35647b90fe02c5e56967fc2fb6f
manishavhanmane00-hash <manishavhanmane00@gmail.com>
2026-03-05 15:39:58 +0530

    App.jsx

--- 3 Month March/CRUD Project/Backend/server.js
+++ 3 Month March/CRUD Project/Backend/server.js
@@ -20,12 +20,38 @@
 
 //Arrow Function - Without use Function keyword.
 
-const getData = () => {
+// const getData = () => {
 
-}
+//}
 
 //Normal Function - Use Function keyword.
 
-function getData() {
+// function getData() {
 
-}
+// }
+
+
+console.log("Hello Node JS Project Started")
+
+
+const express = require('express')  //Node js framework
+const app = express() // app - variable - store express function
+const mongoose = require('mongoose')  // Library - connect mongodb database
+const { stringify } = require('querystring')
+app.use(express.json())  // Convert all data into json format 
+
+// DB Connect
+
+mongoose.connect("mongodb://localhost:27017/item-database").then( () => console.log("Mpngo DB Connected") ).catch( (error) => console.log(error) )  
+
+// Schema - Model - Data base table structure
+// values store database - structure 
+
+const itemsSchema = new mongoose.Schema({
+    name : String,
+    description : String,
+    sellingPrice : Number
+})
+
+
+const Items = new mongoose.model("Items", itemsSchema)  // Table Name  // Collection Name - Items
